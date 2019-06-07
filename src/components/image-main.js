e8ed6066108ae57721d6dbca214933a6ce1afde2
Sasha Miller <sashazm85@gmail.com>
2019-06-07 15:37:58 -0700

    main image component

---
+++ src/components/image-main.js
@@ -0,0 +1,10 @@
+import React from "react"
+import poster from "../images/creativity-small.png"
+
+const ImageMain = ({}) => (
+  <div className="site__container-featured-image">
+    <div className="featured-image"></div>
+  </div>
+)
+
+export default ImageMain
--- src/components/layout.js
+++ src/components/layout.js
@@ -8,12 +8,11 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
-
+import ImageMain from "../components/image-main"
 import Header from "./header"
 // reset before applying your styles
 import "../sass/normalize.scss"
 import "../sass/styles.scss"
-
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -28,75 +27,90 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       // <!-- Site Navigation Area Start-->
-      <div class="site__container">
-        <div class="sidebar">
-        <section class="sidebar__nav" tabindex="0">
-          {/* <!-- Header Area Start--> */}
-            <div class="site__info" >
-              <h1 class="site__title" >Sashas Portfolio</h1>
-              <p class="site__description">
+      <div className="site__container">
+        <div className="sidebar">
+          <section className="sidebar__nav" tabindex="0">
+            {/* <!-- Header Area Start--> */}
+            <div className="site__info">
+              <h1 className="site__title">Sashas Portfolio</h1>
+              <p className="site__description">
                 I'm a multilingual, based in Sacramento area.
               </p>
             </div>
 
             {/* <!-- Menu Links Start --> */}
-            <nav class="menu__container">
-              <ul class="menu menu--active">
-                <li class="menu__item">
-                  <a href="index.html" class="current menu__link">Home</a>
-                  <div class="wave-container">
-                    <div class="wave"></div>
-                    <div class="wave wave--second"></div>
-                    <div class="wave wave--third"></div>
+            <nav className="menu__container">
+              <ul className="menu menu--active">
+                <li className="menu__item">
+                  <a href="index.html" className="current menu__link">
+                    Home
+                  </a>
+                  <div className="wave-container">
+                    <div className="wave"></div>
+                    <div className="wave wave--second"></div>
+                    <div className="wave wave--third"></div>
                   </div>
                 </li>
-                <li class="menu__item">
-                  <a href="about.html" class="menu__link">About</a>
-                  <div class="wave-container">
-                    <div class="wave"></div>
-                    <div class="wave wave--second"></div>
-                    <div class="wave wave--third"></div>
+                <li className="menu__item">
+                  <a href="about.html" className="menu__link">
+                    About
+                  </a>
+                  <div className="wave-container">
+                    <div className="wave"></div>
+                    <div className="wave wave--second"></div>
+                    <div className="wave wave--third"></div>
                   </div>
                 </li>
-                <li class="menu__item"><a href="scroll.html" class="menu__link">Hobbies</a></li>
-                <li class="menu__item"><a href="contact.html" class="menu__link">Resume</a></li>
+                <li className="menu__item">
+                  <a href="scroll.html" className="menu__link">
+                    Hobbies
+                  </a>
+                </li>
+                <li className="menu__item">
+                  <a href="contact.html" className="menu__link">
+                    Resume
+                  </a>
+                </li>
               </ul>
             </nav>
             {/* <!-- Footer Area Start --> */}
-          <footer>
-            <div class="site__footer">
-              © 2018.
+            <footer>
+              <div className="site__footer">© 2018.</div>
+            </footer>
+          </section>
+
+          {/* <!-- Media Links Area Start--> */}
+          <section className="sidebar__social">
+            {/* <!-- Social Media Links --> */}
+            {/* <!-- Menu Toggle --> */}
+            <div className="nav-toggle">
+              <div className="nav-toggle__icon">1</div>
             </div>
-          </footer>
-        </section>
-
-        {/* <!-- Media Links Area Start--> */}
-        <section class="sidebar__social">
-
-          {/* <!-- Social Media Links --> */}
-           {/* <!-- Menu Toggle --> */}
-          <div class="nav-toggle">
-            <div class="nav-toggle__icon">1</div>
-          </div>
-          <a href="#" class="social-icon email"><i class="fas fa-at"></i>2</a>
-          <a href="#" class="social-icon github"><i class="fab fa-github"></i>3</a>
-          <a href="#" class="social-icon drupal"><i class="fab fa-drupal"></i>4</a>
-          <a href="#" class="social-icon codepen"><i class="fab fa-codepen"></i>5</a>
-          <a href="#" class="social-icon codecamp"><i class="fa fa-free-code-camp"></i>6</a>
-
-        </section>
-        </div> {/*<!-- sidebar end --> */}
+            <a href="#" className="social-icon email">
+              <i className="fas fa-at"></i>2
+            </a>
+            <a href="#" className="social-icon github">
+              <i className="fab fa-github"></i>3
+            </a>
+            <a href="#" className="social-icon drupal">
+              <i className="fab fa-drupal"></i>4
+            </a>
+            <a href="#" className="social-icon codepen">
+              <i className="fab fa-codepen"></i>5
+            </a>
+            <a href="#" className="social-icon codecamp">
+              <i className="fa fa-free-code-camp"></i>6
+            </a>
+          </section>
+        </div>{" "}
+        {/*<!-- sidebar end --> */}
         {/* <!-- Media Links Area End--> */}
         {/* <!-- Site Navigation Area End--> */}
-
         {/* <!-- Site Main Content Area Start--> */}
-        <main class="content">{children}</main>
+        <main className="content">{children}</main>
         {/* // <!-- Featured Image Start --> */}
-      <div class="site__container-featured-image">
-        <div class="featured-image"></div>
-      </div>
-      {/* // <!-- Featured Image End --> */}
-
+        <ImageMain />
+        {/* // <!-- Featured Image End --> */}
       </div>
     )}
   />
