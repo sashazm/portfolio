822cd9efaeb0a4a290e2601c9a37acabf54c1868
Sasha Miller <sashazm85@gmail.com>
2019-06-15 23:12:42 -0700

    main image responsive

--- src/components/image-main.js
+++ src/components/image-main.js
@@ -1,10 +1,32 @@
 import React from "react"
-import poster from "../images/creativity-small.png"
+// import poster from "../images/creativity-small.png"
 
-const ImageMain = ({}) => (
-  <div className="site__container-featured-image">
-    <div className="featured-image"></div>
-  </div>
-)
+const ImageMain = () => {
+  return (
+    <>
+      <div className="featured-image">
+        <div>
+          {/* Curved Text */}
+          <svg
+            viewBox="-15 100 500 100"
+            preserveAspectRatio="xMinYMid"
+            className="curved-text"
+          >
+            <path
+              className="curved-text__curve-transparent"
+              id="curve"
+              d="M16.2,148.6c4-6.1,65.5-96.8,178.6-92.3c101.3,1.2,130.8,90.3,175.1,97"
+            />
+            <text>
+              <textPath xlinkHref="#curve" className="curved-text__text">
+                Creativity is intelligence having fun
+              </textPath>
+            </text>
+          </svg>
+        </div>
+      </div>
+    </>
+  )
+}
 
 export default ImageMain
