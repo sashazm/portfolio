86e8b80c6248642dbc420857221c227b146b5f4e
Sasha Miller <sashazm85@gmail.com>
2019-06-20 13:27:36 -0700

    case study responsive layout

--- src/components/case-study.js
+++ src/components/case-study.js
@@ -11,9 +11,9 @@ const CaseStudy = () => (
   <section className="case-study">
     <hr className="line" />
     <h2 className="case-study__title case-study__sub-title--alt">Case Study</h2>
-    <h3 className="case-study__title case-study__title-big">
+    <h2 className="case-study__title case-study__title-big">
       Akame hair salon
-    </h3>
+    </h2>
     <div className="case-study__image-container">
       <AkameBackground />
       <a
@@ -27,15 +27,28 @@ const CaseStudy = () => (
         <span className="image-link__text u-centered">Site Demo</span>
       </a>
     </div>
-
+    <h3 className="case-study__title case-study__sub-title">
+      Lorem ipsum about lorem ipsum ☺
+    </h3>
     <p className="case-study__text">
-      <h3 className="case-study__title case-study__sub-title">
-        I am an h3 heading, this is how i look
-      </h3>
-      Multiple named cell tokens within and between rows create a single named
-      grid area that spans the corresponding grid cells. Unless those cells form
-      a rectangle, the declaration is invalid.
+      Lorem ipsum is a pseudo-Latin text used in web design, typography, layout,
+      and printing in place of English to emphasise design elements over
+      content. It's also called placeholder (or filler) text. It's a convenient
+      tool for mock-ups. It helps to outline the visual elements of a document
+      or presentation, eg typography, font, or layout. Lorem ipsum is mostly a
+      part of a Latin text by the classical author and philosopher Cicero.
     </p>
+    <h3 className="case-study__title case-study__sub-title">
+      I don't know how I would feel if in the future my speech was used to
+      represent a bunch of nonsense
+    </h3>
+    Its words and letters have been changed by addition or removal, so to
+    deliberately render its content nonsensical; it's not genuine, correct, or
+    comprehensible Latin anymore. While lorem ipsum's still resembles classical
+    Latin, it actually has no meaning whatsoever. As Cicero's text doesn't
+    contain the letters K, W, or Z, alien to latin, these, and others are often
+    inserted randomly to mimic the typographic appearence of European languages,
+    as are digraphs not to be found in the original.
   </section>
 )
 
