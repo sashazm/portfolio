3f78b910cf45b1a875ce3b94a93ae5706681f990
Sasha Miller <sashazm85@gmail.com>
2019-07-23 13:20:50 -0700

    testing

--- src/components/contact-info.js
+++ src/components/contact-info.js
@@ -2,8 +2,8 @@ import React from "react"
 
 const ContactInfo = () => (
   <footer id="contact-info" className="contact-info l-container">
-    {/* <p className="contact-info__email--alt">Contact me</p>
-    <p className="contact-info__email">sashazm85@gmail.com</p> */}
+    <p className="contact-info__email--alt">Contact me</p>
+    <p className="contact-info__email">sashazm85@gmail.com</p>
   </footer>
 )
 
