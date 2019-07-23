7e0fdd8e36d7fc1e8786aecaef637a22e613d57f
Sasha Miller <sashazm85@gmail.com>
2019-07-22 23:15:46 -0700

    page not found styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,10 +3,16 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import { FaCommentAlt } from "react-icons/fa"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
+    <div className="content-divider content-divider--not-found">
+      <FaCommentAlt className="content-divider__icon content-divider__icon--contact" />
+      <h1 className="content-divider__text content-divider__text--not-found">
+        NOT FOUND
+      </h1>
+    </div>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
