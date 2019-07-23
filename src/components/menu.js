55de37265d0cbcc7ac56b9deaf4c868daadda967
Sasha Miller <sashazm85@gmail.com>
2019-07-23 13:05:48 -0700

    menu link to the resume

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,9 +1,10 @@
 import React from "react"
+import resume from "../files/sasha_miller_resume.pdf"
 
 const Menu = () => (
   <ul className="menu u-centered menu--active">
     <li className="menu__item">
-      <a href="#about" className="current menu__link nav-link">
+      <a href="/#about" className="current menu__link nav-link">
         About
       </a>
       <div className="wave-container">
@@ -13,7 +14,7 @@ const Menu = () => (
       </div>
     </li>
     <li className="menu__item">
-      <a href="#projects" className="menu__link nav-link">
+      <a href="/#projects" className="menu__link nav-link">
         Projects
       </a>
       <div className="wave-container">
@@ -23,7 +24,7 @@ const Menu = () => (
       </div>
     </li>
     <li className="menu__item">
-      <a href="#resume" className="menu__link nav-link">
+      <a href={resume} className="menu__link nav-link">
         Resume
       </a>
       <div className="wave-container">
@@ -33,7 +34,7 @@ const Menu = () => (
       </div>
     </li>
     <li className="menu__item">
-      <a href="#contact-info" className="menu__link nav-link">
+      <a href="/#contact-info" className="menu__link nav-link">
         Contact
       </a>
       <div className="wave-container">
