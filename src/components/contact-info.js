5c0c1e5d0df7a2029a7b789b31a13af36553e2db
Sasha Miller <sashazm85@gmail.com>
2019-07-23 13:12:41 -0700

    testing width in mobile

--- src/components/contact-info.js
+++ src/components/contact-info.js
@@ -1,9 +1,9 @@
 import React from "react"
 
 const ContactInfo = () => (
-  <footer id="contact-info" className="contact-info">
-    <p className="contact-info__email--alt">Contact me</p>
-    <p className="contact-info__email">sashazm85@gmail.com</p>
+  <footer id="contact-info" className="contact-info l-container">
+    {/* <p className="contact-info__email--alt">Contact me</p>
+    <p className="contact-info__email">sashazm85@gmail.com</p> */}
   </footer>
 )
 
