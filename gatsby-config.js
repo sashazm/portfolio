be2bb1b3edea6cb5c0a73840bf2ba0fe5235d0ce
Sasha Miller <sashazm85@gmail.com>
2019-07-23 14:59:03 -0700

    adress bar color

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
-        theme_color: `#000000`,
+        theme_color: `#eac743`,
         display: `minimal-ui`,
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
